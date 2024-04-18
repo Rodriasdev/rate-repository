3de19f36d78477e9e686029e882dc8755bd7b662
Rodriasd <veronrodrigo98@gmail.com>
2024-04-17 22:17:40 -0300

    was added the component main and the data with the repositories

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import Constants from 'expo-constants'
+
+export const Main = () => {
+    return(
+        <View style={{marginTop: Constants.statusBarHeight, flexGrow: 1}}>
+            <Text>Rate Repository Application</Text>
+        </View>
+    )
+}
