2055e467185a7cc515f393d3c12d0930b18589fa
Rodriasd <veronrodrigo98@gmail.com>
2024-04-18 21:42:43 -0300

    was modifiqued the list of the repositories and remove the api of github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,33 +1,26 @@
-import React, { useState,useEffect } from "react";
+
 import { View,Text } from "react-native";
 import repositories from "../data/repositories.js";
-import { getOneUsers } from "../api/GitHubApi.js";
 
 export const RepositoryList = () => {
-    const [stateRepositories, setStateRepositories] = useState([]) 
 
-    useEffect(() => { 
-        ( 
-            async () => {
-                const userData = await Promise.all(repositories.map(async (repo) => {
-                    return await getOneUsers(repo)
-                }))
-                setStateRepositories(userData)
-            }
-        )()
-    },[])
+
 
     return(
         <View>
             {
-                stateRepositories.map((repo) => (
+                repositories.map((repo) => (
                     <View key={repo.id}>
-                        <Text>{repo.login}</Text>
-                        <Text>{repo.bio == null ? "Sin biografía" : repo.bio}</Text>
-                        <Text>{repo.public_repos}</Text>
-                        <Text>{repo.public_gists}</Text>
-                        <Text>{repo.followers}</Text>
-                        <Text>{repo.following}</Text>
+                        <Text>Username: {repo.login}</Text>
+                        <Text>Biography: {repo.bio == null ? "sin biography" : repo.bio}</Text>
+                        <Text>Repositories publics:{repo.public_repos}</Text>
+                        <Text>Lenguage:{repo.lenguage}</Text>
+                        <Text>Followers: {repo.followers}</Text>
+                        <Text>Followings: {repo.following}</Text>
+                        <Text>Stars:{repo.stars}</Text>
+                        <Text>Review:{repo.reviewCount}</Text>
+                        <Text>Location: {repo.location  == null ? "sin location" : repo.location}</Text>
+                        <Text>Rating:{repo.rating}</Text>
                     </View>
                 ))
             }
