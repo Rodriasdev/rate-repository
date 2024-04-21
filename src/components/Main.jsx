8ae82abb2bce2c94ad2df9bf9361b9291b2661aa
Rodriasd <veronrodrigo98@gmail.com>
2024-04-20 21:19:30 -0300

    routes added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { RepositoryList } from "./RepositoryList.jsx";
 import { AppBar } from "./AppBar.jsx";
+import { Switch, Route, Redirect } from 'react-router-native';
 
 export const Main = () => {
     return(
         <View style={{flex: 1}}>
-            <AppBar/>
-            <RepositoryList/>
+            <AppBar />
+            <Switch>
+                <Route path="/" exact>
+                    <RepositoryList/>
+                </Route>
+                <Route path="/signin" exact>
+                    <Text>a</Text>
+                </Route>
+                <Redirect to='/'/>
+            </Switch>
         </View>
     )
 }
